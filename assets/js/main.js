aa7fa28baf611a0fb729908466486c6dceea0e6c
DiceglieA <angelodiceglie10@gmail.com>
2022-10-24 16:44:46 +0200

    fine

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,22 @@
 const item = document.querySelector('.table')
-for(let i = 0; i <= 100; i++) {
-    item.innerhtml += `<div class=".square">${i}</div>`
-    console.log('boh')
+for(let i = 1; i <= 100; i++) {
+    //MHitem.innerHTML += `<div class="square">${i}</div>`;
+    //console.log(i)
+
+    if ( i % 3 === 0 && i % 5 === 0){
+        item.innerHTML += `<div class="square fizzbuzz">FizzBuzz</div>`
+        console.log('FizzBuzz')
+
+    } else if ( i  % 3 === 0){
+        item.innerHTML += `<div class="square fizz">Fizz</div>`
+        console.log('Fizz')
+
+    } else if ( i % 5 === 0) {
+        item.innerHTML += `<div class="square buzz">Buzz</div>`
+        console.log('Buzz')
+
+    } else {
+        item.innerHTML += `<div class="square">${i}</div>`
+    };
 }
+
